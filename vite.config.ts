4dd765022ea93b57302d7f0923ad95ef4b07edaa
zhuangweizhan <365638983@qq.com>
2022-06-05 23:49:56 +0800

    🦄 refactor:重新提交预览文件

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       ]
     },
     build: {
-      assetsDir: "./vue3/"
+      assetsDir: "./cb-ui/dist/"
     },
     server: {
       host: "0.0.0.0",
